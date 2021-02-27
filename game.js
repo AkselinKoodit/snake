ee7ec5f26280a38a63ff85499bfe5309b1152de9
Akseli <s2100140@edu.bc.fi>
2021-02-27 14:32:24 +0200

    snake game, ready, firest commit

---
+++ game.js
@@ -0,0 +1,52 @@
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  snakeSpeed,
+  getSnakeHead,
+  eatSelf,
+} from "./snake.js";
+
+import { update as updateFood, draw as drawFood, keepScore } from "./food.js";
+
+import { intoWall } from "./grid.js";
+
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById("field");
+
+function main(currentTime) {
+  if (gameOver) {
+    if (
+      confirm("You lost! Your score: " + keepScore() + ". Press ok to restart.")
+    ) {
+      window.location = "/";
+    }
+    return;
+  }
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / snakeSpeed) return;
+
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+  updateFood();
+  console.log(keepScore());
+  checkDeath();
+}
+
+function draw() {
+  gameBoard.innerHTML = "";
+  drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = intoWall(getSnakeHead()) || eatSelf();
+}
