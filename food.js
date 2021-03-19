7358e8ce5a62050c455d761b539dee1cb55a7d74
Akseli <s2100140@edu.bc.fi>
2021-03-19 16:28:52 +0200

    end sound, game over text field, reload with any key

--- food.js
+++ food.js
@@ -4,9 +4,26 @@ import { randomGridPosition } from "./grid.js";
 let food = getRandomFoodPosition();
 let score = 0;
 const growSpeed = 1;
+let eatsound;
 
 export function update() {
   if (onSnake(food)) {
+    function sound(src) {
+      this.sound = document.createElement("audio");
+      this.sound.src = src;
+      this.sound.setAttribute("preload", "auto");
+      this.sound.setAttribute("controls", "none");
+      this.sound.style.display = "none";
+      document.body.appendChild(this.sound);
+      this.play = function () {
+        this.sound.play();
+      };
+      this.stop = function () {
+        this.sound.pause();
+      };
+    }
+    eatsound = new sound("ballBlib.wav");
+    eatsound.play();
     growSnake(growSpeed);
     score++;
     food = getRandomFoodPosition();
--- game.js
+++ game.js
@@ -13,17 +13,44 @@ import { intoWall } from "./grid.js";
 let lastRenderTime = 0;
 let gameOver = false;
 const gameBoard = document.getElementById("field");
+let endMessageArea = document.getElementById("placeholder");
+let endMessage = document.getElementById("endMessage");
 
 function main(currentTime) {
+  endMessageArea.classList.add("invisible");
   if (gameOver) {
-    if (
-      confirm(
-        "Game over! Your score: " + keepScore() + ". Press ok to restart."
-      )
-    ) {
-      window.location.reload();
+    function sound(src) {
+      this.sound = document.createElement("audio");
+      this.sound.src = src;
+      this.sound.setAttribute("preload", "auto");
+      this.sound.setAttribute("controls", "none");
+      this.sound.style.display = "none";
+      document.body.appendChild(this.sound);
+      this.play = function () {
+        this.sound.play();
+      };
+      this.stop = function () {
+        this.sound.pause();
+      };
     }
+    let endSound = new sound("mixkit-losing.wav");
+    endSound.play();
+    console.log("Game over!");
+    document.getElementById("score").classList.add("invisible");
+    endMessage.innerText = `Game over! Your score: ${keepScore()} \nPress any button to restart`;
+    endMessageArea.classList.remove("invisible");
+    window.addEventListener("keydown", (e) => {
+      window.location.reload();
+    });
     return;
+
+    // if (
+    //   confirm(
+    //     "Game over! Your score: " + keepScore() + ". Press ok to restart."
+    //   )
+    // ) {
+    //   window.location.reload();
+    // }
   }
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
